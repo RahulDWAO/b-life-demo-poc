673e20e2e32d5752615faae85ca880104d218992
jaytech989 <jay.vyas@techchefz.com>
2025-05-02 21:03:35 +0530

    label change for gendernew

---
+++ blocks/gendernew/gendernew.js
@@ -0,0 +1,9 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  console.log("gender", block)
+  debugger;
+  /* change to ul, li */
+
+}
