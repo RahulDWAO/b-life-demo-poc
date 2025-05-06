c7d0994d28a0c424fcaa0ca514d07cc077505c09
jaytech989 <jay.vyas@techchefz.com>
2025-05-06 22:36:58 +0530

    spred sheet compoent addede

---
+++ blocks/spreedsheetfragment/spreedsheetfragment.js
@@ -0,0 +1,14 @@
+export default async function decorate(block) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  console.log(path,"path")
+//   const fragment = await loadFragment(path);
+//   if (fragment) {
+//     const fragmentSection = fragment.querySelector(':scope .section');
+//     if (fragmentSection) {
+//       block.classList.add(...fragmentSection.classList);
+//       block.classList.remove('section');
+//       block.replaceChildren(...fragmentSection.childNodes);
+//     }
+//   }
+}
